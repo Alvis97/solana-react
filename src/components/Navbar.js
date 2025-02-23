08d8e882e90f3b8be88a2b744a326510efe80a6a
Alvis97 <101591973+Alvis97@users.noreply.github.com>
2025-02-23 21:18:29 +0100

    Homepage

---
+++ src/components/Navbar.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import navStyle from '../styles/Navbar.module.scss';
+import { useState } from 'react';
+
+function Navbar() {
+
+   const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <div className={navStyle.parent}>
+       <p>Solana With React</p>
+
+       <button className={navStyle.hamburger} onClick={() => setIsOpen(!isOpen)}>
+       <div className={`${navStyle.bar} ${isOpen ? navStyle.barOpen : ''}`}></div>
+        <div className={`${navStyle.bar} ${isOpen ? navStyle.barOpen : ''}`}></div>
+        <div className={`${navStyle.bar} ${isOpen ? navStyle.barOpen : ''}`}></div>
+       </button>
+  
+      <ul className={`${navStyle.menu} ${isOpen ? navStyle.open : ''}`}>
+        <li><a href="/">Home</a></li>
+        <li><a href="/about">About</a></li>
+        <li><a href="/contact">Contact</a></li>
+      </ul>
+      
+    </div>
+  )
+}
+
+export default Navbar
